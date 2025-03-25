6ce9b3577c6634839037f2e2fd883cc1857d6b88
Aditya Dutta <148987900+Adi-1025@users.noreply.github.com>
2025-03-25 11:45:57 +0530

    Add files via upload

---
+++ One_Click_Magic.bat
@@ -0,0 +1,6 @@
+@echo off
+start https://www.google.com
+start https://www.youtube.com
+start https://www.instagram.com
+start https://www.kaggle.com/datasets/mrsimple07/clothes-price-prediction
+exit
